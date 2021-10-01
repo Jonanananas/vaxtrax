5782d973035205580e775e9d250d459317325e88
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2021-10-01 21:48:47 +0300

    Make a user info class and an activity for editing the class' attributes

--- app/src/main/java/com/example/vaxtrax/MainActivity.java
+++ app/src/main/java/com/example/vaxtrax/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.vaxtrax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.set_user_info);
+    }
+    public void onButtonClicked(View v){
+        switch(v.getId()) {
+            case R.id.button_enterInfo:
+                Log.d("DEBUGGING", "button_enterInfo pressed");
+//                Intent intent = new Intent(this, MainMenu.class);
+                EditText editTextFirstName = (EditText) findViewById(R.id.editText_firstName);
+                EditText editTextLastName = (EditText) findViewById(R.id.editText_lastName);
+                UserInfo.setFirstName(editTextFirstName.getText().toString());
+                UserInfo.setLastName(editTextLastName.getText().toString());
+//                startActivity(intent);
+                break;
+        }
     }
 }

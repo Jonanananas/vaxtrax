a37ffa7ae99fbe058d3130894a5a1bf414174b0a
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2021-10-03 12:38:09 +0300

    Remove unused import

--- app/src/main/java/com/example/vaxtrax/MainActivity.java
+++ app/src/main/java/com/example/vaxtrax/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {

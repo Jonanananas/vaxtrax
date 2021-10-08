2a578ba498a45cee6af82930a89c89d259a2e865
jackthelantern <naimanowrin123@gmail.com>
2021-10-08 16:15:58 +0300

    addvax acvitity added

--- app/src/main/java/com/example/vaxtrax/AddVax.java
+++ app/src/main/java/com/example/vaxtrax/AddVax.java
@@ -1,17 +1,26 @@
 package com.example.vaxtrax;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.SearchManager;
+
+import android.widget.EditText;
 import android.widget.SearchView;
-import android.widget.SearchView.OnQueryTextListener;
 import android.os.Bundle;
 
 public class AddVax extends AppCompatActivity {
-
+    SearchView search;
+    EditText VaxName;
+    EditText VaxDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vax);
-        SearchView search = findViewById(R.id.vaccSearch);
+
+        search = findViewById(R.id.vaccSearch);
+        VaxName = findViewById(R.id.vaccEditText);
+        /*VaxDate = findViewById(R.id.vaccDate);*/
+
+
+
+
     }
 }

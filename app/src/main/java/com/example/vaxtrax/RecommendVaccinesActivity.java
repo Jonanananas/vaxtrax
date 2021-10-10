e67faf077e1b3fe20c92119a63da1e1d661da564
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2021-10-10 13:09:59 +0300

    Add comments, do formatting and add a button to go back to MainActivity

--- app/src/main/java/com/example/vaxtrax/RecommendVaccinesActivity.java
+++ app/src/main/java/com/example/vaxtrax/RecommendVaccinesActivity.java
@@ -18,8 +18,11 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+// An activity which displays recommended vaccines which the user should have got at their age.
+// Vaccines information source:
+// https://thl.fi/fi/web/infektiotaudit-ja-rokotukset/tietoa-rokotuksista/kansallinen-rokotusohjelma/rokotusohjelma-lapsille-ja-aikuisille#rokotusohjelma
+
 public class RecommendVaccinesActivity extends AppCompatActivity {
-    private ArrayList recommendedVaccines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +30,12 @@ public class RecommendVaccinesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recommend_vaccines);
 
         SharedPreferences prefs = getSharedPreferences("com.example.vaxtrax_userInfo", MODE_PRIVATE);
-        int userAgeMonths = prefs.getInt("userAgeMonths", 780);
-        Log.d("DEBUGGING", "userAgeMonths: " + userAgeMonths);
-        int userAgeYears = prefs.getInt("userAge", 65);
         TextView textView_recommendedGreeting = findViewById(R.id.textView_recommendedGreeting);
+        int userAgeMonths = prefs.getInt("userAgeMonths", 780);
+        int userAgeYears = prefs.getInt("userAge", 65);
 
-        recommendedVaccines = new ArrayList();
+//        Determine the displayed recommended vaccines based on the user's age
+        ArrayList<String> recommendedVaccines = new ArrayList<>();
         if(userAgeMonths >= 2)
             recommendedVaccines.add("Rotavirus");
         if(userAgeMonths >= 3) {
@@ -55,8 +58,11 @@ public class RecommendVaccinesActivity extends AppCompatActivity {
         textView_recommendedGreeting.setText(
                 getString(R.string.recommended_vaccines_greeting_text, userAgeYears, userAgeMonths - userAgeYears*12)
         );
-
         ListView lv = findViewById(R.id.listView_recommendedVaccines);
-        lv.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, recommendedVaccines));
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recommendedVaccines));
+    }
+    public void onButtonClicked(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

92c9a76f6bb31e42f50dbf88a74ff3625235dafe
johanlav <johannalavikainen@gmail.com>
2021-10-13 22:52:19 +0300

    fix javadoc comments

--- app/src/main/java/com/example/vaxtrax/VaccinationsByCountry.java
+++ app/src/main/java/com/example/vaxtrax/VaccinationsByCountry.java
@@ -7,8 +7,6 @@ import java.util.List;
  * Class for arraylist of vaccinations by country
  * @author Johanna Lavikainen
  * @version 1.0 13/10/2021
- * @param String country name of country
- * @param ArrayList vaccinations, vaccinations by country
  */
 public class VaccinationsByCountry {
 
@@ -18,11 +16,17 @@ public class VaccinationsByCountry {
     public String getCountry() {
         return this.country;
     }
+
     public VaccinationsByCountry(String country, VaccineInfo ...name) {
         this.country = country;
         List <VaccineInfo> vaccinationsList = Arrays.asList(name);
         this.vaccinations = new ArrayList<VaccineInfo>(vaccinationsList);
     }
+
+    /**
+     * Get vaccinations
+     * @return ArrayList<VaccineInfo> which contains list of vaccinations
+     */
     public ArrayList<VaccineInfo> getVaccinations() {
         return this.vaccinations;
     }

b57ca5217daa1c1cf34ca3900b11f8184e269f20
jackthelantern <naimanowrin123@gmail.com>
2021-10-13 22:57:25 +0300

    java doc changes

--- app/src/main/java/com/example/vaxtrax/JsonData.java
+++ app/src/main/java/com/example/vaxtrax/JsonData.java
@@ -5,8 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 /** Class is created to make objects out of given data
  * @author Naima Nowrin
- * @version 1.0 13/10/2021
- * @param String name, String date takes vaccine name and date as parameter*/
+ * @version 1.0 13/10/2021*/
 public class JsonData {
     @SerializedName("name")
     private String name;
@@ -14,10 +13,13 @@ public class JsonData {
     private String date;
     private ArrayList<String>list;
 
+    /**@param String name is user given vaccine name
+     * @param String date is user given date of when vaccine was taken */
     public JsonData(String name, String date) {
         this.name = name;
         this.date = date;
     }
+
 
     public String getName() {
         return name;
@@ -31,17 +33,7 @@ public class JsonData {
         return list;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setList(ArrayList<String> list) {
-        this.list = list;
-    }
 
     @Override
     public String toString() {
